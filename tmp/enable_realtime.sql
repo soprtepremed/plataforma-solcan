3fe37d75a50fb180c2eb2b9bef4e17e73a3c4fa1
Carlos Ruiz <carlosadolforuizlopez@gmail.com>
2026-04-08 21:26:22 -0600

    Security: Purged all scripts with exposed credentials from Git tracking

--- tmp/enable_realtime.sql
+++
@@ -1,8 +0,0 @@
--- Enable Realtime for notificaciones table
-begin;
-  -- remove the table from the replication if it exists
-  alter publication supabase_realtime drop table if exists notificaciones;
-
-  -- add the table to the replication
-  alter publication supabase_realtime add table notificaciones;
-commit;
